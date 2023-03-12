7729217f523f49c2d46e4cff7aff1d1d072bfc36
Robson <robccorreia@gmail.com>
2023-03-12 19:40:00 -0300

    feat: desafio 1

---
+++ concepts/store/configureStore.js
@@ -0,0 +1,11 @@
+import aluno from "./reducers/aluno.js";
+import aulas from "./reducers/aulas.js";
+
+const reducer = Redux.combineReducers({ aluno, aulas });
+
+const store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
