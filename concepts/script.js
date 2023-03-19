af6488b134da1efcb90a115a5c259a986dd80db9
Robson <robccorreia@gmail.com>
2023-03-19 16:15:28 -0300

    feat: localStorage

--- concepts/script.js
+++ concepts/script.js
@@ -1,6 +1,14 @@
+function getLocalStorage(key, initial) {
+  try {
+    return JSON.parse(window.localStorage.getItem(key));
+  } catch (error) {
+    return initial;
+  }
+}
+
 const initialState = {
   loading: false,
-  data: null,
+  data: getLocalStorage("data", null),
   error: null,
 };
 
@@ -37,12 +45,23 @@ const thunk = (store) => (next) => (action) => {
   }
 };
 
+const localStorage = (store) => (next) => (action) => {
+  const result = next(action);
+  if (action.localStorage !== undefined) {
+    window.localStorage.setItem(
+      action.localStorage,
+      JSON.stringify(action.payload)
+    );
+  }
+  return result;
+};
+
 const { applyMiddleware, compose } = Redux;
 
 const composeEnchancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = composeEnchancers(applyMiddleware(thunk));
+const enhancer = composeEnchancers(applyMiddleware(thunk, localStorage));
 
 const store = Redux.createStore(reducer, enhancer);
 
@@ -52,11 +71,14 @@ function fetchUrl(url) {
     try {
       dispatch({ type: "FETCH_STARTED" });
       const data = await fetch(url).then((r) => r.json());
-      dispatch({ type: "FETCH_SUCCESS", payload: data });
+      dispatch({ type: "FETCH_SUCCESS", payload: data, localStorage: "data" });
     } catch (error) {
       dispatch({ type: "FETCH_ERROR", payload: error.message });
     }
   };
 }
 
-store.dispatch(fetchUrl("https://dogsapi.origamid.dev/json/api/photo"));
+const state = store.getState();
+if (state.data === null) {
+  store.dispatch(fetchUrl("https://dogsapi.origamid.dev/json/api/photo"));
+}
